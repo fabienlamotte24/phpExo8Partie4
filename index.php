70d6303379d1a9ef2c245cb4554cbf5e2fc4e585
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 11:52:55 +0200

    exo8p4 php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  function calcul($number1, $number2, $number3){
+    echo $number1 + $number2 + $number3;
+  }
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+<?= calcul(125,25,364); ?>
+    </p>
+  </body>
+</html>
